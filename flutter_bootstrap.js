ae2dfbf40f0859a893e0bd91cfe8dd25802621bf
Artem Kurochkin <ArtemQ98@yandex.ru>
2026-02-26 23:03:33 +0300

    add notes

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"c29809135135e262a912cf583b2c90deb9ded6
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "158256212"
+    serviceWorkerVersion: "2148556956"
   }
 });
